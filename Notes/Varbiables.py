03adbddd686888cdca9d2f35af139c6eb42289e9
Yoyoshiuki <yossel.viera@ucas-edu.net>
2026-01-22 11:28:45 -0700

    frist commit

--- Notes/Varbiables.py
+++
@@ -1,4 +0,0 @@
-
-name = "Tia"
-
-print(name)
---
+++ Practices/Varbiables.py
@@ -0,0 +1,13 @@
+# YV, Variables Practice
+
+name = "Yossel"
+number = 60
+number2 = 250
+bk = "Eggs"
+fc = "Green"
+sn = "UCAS"
+y = "2020"
+ec = "Brown"
+a = "15"
+fs = "band class at phs"
+print(name, number, number2, bk, fc, sn, y, ec, a, fs)
