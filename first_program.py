e4af58fa45bf3406ffdb7c4568381e9824d4d2bb
Yoyoshiuki <yossel.viera@ucas-edu.net>
2026-01-23 11:12:11 -0700

    second commit

--- first_program.py
+++ first_program.py
@@ -3,3 +3,10 @@ if mcb == 'yes':
    print("YAYY!!")
 else:
   print("NOOOO!!! Whyyyyyy??")
+
+
+Ex = input("Explain yoursefl! :")
+if Ex == 'no' or "No" :
+ print("Bad boy!")
+else:
+   print("That's a good excuse i guess...")
